ad3af068b8e0874749f45c4381be6f3c089385d7
nabi <gadimovnabi@gmail.com>
2021-04-07 00:30:31 +0500

    ui тест который не запусается хз поч

--- app/src/test/java/com/example/retrofit_example/UITest.kt
+++ app/src/test/java/com/example/retrofit_example/UITest.kt
@@ -1,5 +1,20 @@
 package com.example.retrofit_example
 
+import org.junit.Rule
+import org.junit.Test
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
 class UITest {
-    //TODO: сделать тестик на проверку recycleview или типа того, или например на проверку бэе цвета
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun isRecycleWork() {
+        onView(withId(R.id.listRv)).check(matches(isDisplayed()))
+    }
 }
