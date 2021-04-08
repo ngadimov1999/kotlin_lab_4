dfa3177afb75d7558ce59526dea42abbbc0bcff8
nabi <gadimovnabi@gmail.com>
2021-04-08 23:24:52 +0500

    доделана xml для начального экрана

--- app/src/main/java/com/example/retrofit_example/models/Pokemon.kt
+++ app/src/main/java/com/example/retrofit_example/models/Pokemon.kt
@@ -1,6 +1,11 @@
 package com.example.retrofit_example.models
 
+import android.media.Image
+
 data class Pokemon(
     val url: String,
-    val name: String
+    val name: String,
+    val power: String,
+    val weight: String,
+    val image: Image //8-10 строки я обделался, потому что хз как брать данные с ссылки))))
 )
--- app/src/test/java/com/example/retrofit_example/UITest.kt
+++ app/src/test/java/com/example/retrofit_example/UITest.kt
@@ -3,18 +3,23 @@ package com.example.retrofit_example
 import org.junit.Rule
 import org.junit.Test
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 
+
 class UITest {
+
     @get:Rule
     var activityRule: ActivityScenarioRule<MainActivity> =
         ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun isRecycleWork() {
-        onView(withId(R.id.listRv)).check(matches(isDisplayed()))
+    fun isListShown() {
+        //onView(withId(R.id.listRv)).check(doesNotExist())
+        //onView(withId(R.id.listRv)).check(matches(isEnabled()))
+        onView(withId(R.id.title)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 }
